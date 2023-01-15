a64bf1470898fab3d7bb0b943889ace2ecc914ed
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-15 00:01:53 -0600

    App: Create a (debatable) initial project structure

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddBookComponent } from './pages/add-book/add-book.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddBookComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
