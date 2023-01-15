3e0e6dddc9120392300aa511324ee347d4bdc90f
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-15 14:18:17 -0600

    App: Minor code cleanups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AddBookComponent } from './home/pages/add-book/add-book.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
 ];
 
