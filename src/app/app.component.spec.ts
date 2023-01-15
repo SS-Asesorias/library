4fb603f412ad642d08c381c64248dec9fcc612e1
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-14 21:00:13 -0600

    App: Basic changes for tauri to work properly

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'my-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('library');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('library app is running!');
   });
 });
