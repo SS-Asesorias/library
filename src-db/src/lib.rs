5a70218236e24577eac190ac053b145a43fc1333
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-15 18:57:16 -0600

    Database: Fix missing relationships when adding new books with authors

--- src-db/src/lib.rs
+++ src-db/src/lib.rs
@@ -1,8 +1,8 @@
-use crate::models::authors::{_NewAuthor, NewAuthor};
-use crate::models::books::{NewBook, _NewBook};
-use crate::schema::authors::dsl::authors;
+use crate::models::authors::{Author, _NewAuthor};
+use crate::models::authors_books::AuthorsBooks;
+use crate::models::books::{Book, NewBook, _NewBook};
 use diesel::sqlite::SqliteConnection;
-use diesel::{Connection, RunQueryDsl};
+use diesel::{CombineDsl, Connection, RunQueryDsl};
 use dotenvy::dotenv;
 use std::{env, usize};
 
@@ -21,21 +21,21 @@ pub fn register_book(conn: &mut SqliteConnection, book: &NewBook) {
     let insertable_book = _NewBook {
         title: match &book.title {
             None => None,
-            Some(val) => Some(val.as_str())
+            Some(val) => Some(val.as_str()),
         },
         editorial: match &book.editorial {
             None => None,
-            Some(val) => Some(val.as_str())
+            Some(val) => Some(val.as_str()),
         },
         edition: match &book.edition {
             None => None,
-            Some(val) => Some(val.as_str())
+            Some(val) => Some(val.as_str()),
         },
         condition: book.condition,
         position: book.position.as_str(),
     };
 
-    create_book(conn, &insertable_book);
+    let inserted_book = create_book(conn, &insertable_book);
 
     for author in &book.authors {
         match author.id {
@@ -44,34 +44,49 @@ pub fn register_book(conn: &mut SqliteConnection, book: &NewBook) {
                     name: author.name.as_str(),
                     lname: match &author.lname {
                         None => None,
-                        Some(val) => Some(val.as_str())
-                    }
+                        Some(val) => Some(val.as_str()),
+                    },
                 };
 
-                create_author(conn, &insertable_author);
-            }
-            _ => {}
-        }
+                let inserted_author = create_author(conn, &insertable_author);
 
-        // TODO: insert relationships between book and authors
+                create_author_book(conn, inserted_author.id, inserted_book.id);
+            }
+            Some(id) => {
+                create_author_book(conn, id, inserted_book.id);
+            }
+        }
     }
 }
 
-// == FOR INTERNAL ==
-fn create_book(conn: &mut SqliteConnection, new_book: &_NewBook) -> usize {
+fn create_book(conn: &mut SqliteConnection, new_book: &_NewBook) -> Book {
     use crate::schema::books;
 
     return diesel::insert_into(books::table)
         .values(new_book)
-        .execute(conn)
+        .get_result::<Book>(conn)
         .expect("Error saving new book");
 }
 
-fn create_author(conn: &mut SqliteConnection, new_author: &_NewAuthor) -> usize {
+fn create_author(conn: &mut SqliteConnection, new_author: &_NewAuthor) -> Author {
     use crate::schema::authors;
 
     return diesel::insert_into(authors::table)
         .values(new_author)
-        .execute(conn)
+        .get_result::<Author>(conn)
         .expect("Error saving author");
+}
+
+fn create_author_book(conn: &mut SqliteConnection, author: i32, book: i32) -> usize {
+    use crate::schema::authors_books;
+
+    let new_relation = AuthorsBooks {
+        author_id: author,
+        book_id: book,
+    };
+
+    return diesel::insert_into(authors_books::table)
+        .values(new_relation)
+        .execute(conn)
+        .expect("Error saving relationship");
 }
