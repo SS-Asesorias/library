1a1faf541fd264c574146248884ad9e55b4736be
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-16 17:27:11 -0600

    App: Create Book service with function to add new books to database

---
+++ src/app/core/services/books.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { invoke } from '@tauri-apps/api/tauri';
+import { Book } from '../models/Book';
+import { Author } from '../models/Author';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  constructor() {}
+
+  async registerBook(book: Book, authors: Author[]) {
+    const newBook = {
+      title: book.title,
+      editorial: book.editorial,
+      edition: book.edition,
+      condition: book.condition,
+      position: book.position,
+      authors: authors,
+    };
+
+    await invoke('register_book_command', { newBook: newBook });
+  }
+}
