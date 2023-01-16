a939ced81bbf3c4c473dd75e541f9748e1cfe647
vgarzareyna <vgarzareyna@gmail.com>
2023-01-15 19:40:10 -0600

    added inputs for the rest of the fields in a book

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,10 +7,18 @@ import { HomeRoutingModule } from './home-routing.module';
 // Angular Material components
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {MatInputModule} from "@angular/material/input";
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AddBookComponent, HomeComponent],
-    imports: [CommonModule, HomeRoutingModule, MatCardModule, MatButtonModule, MatInputModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+  ],
 })
 export class HomeModule {}
--- src/app/home/pages/add-book/add-book.component.ts
+++ src/app/home/pages/add-book/add-book.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.scss']
+  styleUrls: ['./add-book.component.scss'],
 })
 export class AddBookComponent {
+  @Input() title: string = '';
+  @Input() editorial: string = '';
+  @Input() edition: string = '';
 
+  @Input() condition: number = 0;
+  @Input() position: string = '';
+
+  logValue(value: string) {
+    console.log(value);
+  }
+
+  addAuthor() {
+    throw Error('Not implemented');
+  }
 }
