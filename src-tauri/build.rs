367d0e1ace7d4453775b2cf2f83eb815f29a1591
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-17 20:03:04 -0600

    Tauri: Bundle database to app and database changes

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
+use std::fs;
+
 fn main() {
+  fs::copy("../src-db/library.sqlite", "../src-tauri/library.sqlite").expect("Failed to copy database to bundle");
+
   tauri_build::build()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,32 @@
 )]
 
 use crate::commands::{hello_world_command, register_book_command};
+use std::fs;
+use std::path::Path;
+use tauri::api::path::app_local_data_dir;
 
 pub mod commands;
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let database_path = app
+                .path_resolver()
+                .resolve_resource("library.sqlite")
+                .expect("Failed to resolve resource: library.sqlite");
+
+            let app_datapath = app_local_data_dir(&app.config()).unwrap();
+            let library_path = format!("{}/library.sqlite", app_datapath.to_str().unwrap());
+
+            let library_file = Path::new(&library_path);
+
+            if !library_file.exists() {
+                fs::copy(database_path.to_str().unwrap(), library_path)
+                    .expect("Failed to create library database");
+            }
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             hello_world_command,
             register_book_command
