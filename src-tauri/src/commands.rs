79ec9ff78530b5225c6a434d94cd0d94a7be87f3
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-18 22:14:20 -0600

    Tauri: Add command to call get_authors() from database

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,19 +1,25 @@
+use src_db::models::authors::Author;
 use src_db::models::books::NewBook;
-use src_db::{establish_connection, register_book};
+use src_db::{establish_connection, get_authors, register_book};
 use tauri::api::path::app_local_data_dir;
-
-#[tauri::command]
-pub async fn hello_world_command(_app: tauri::AppHandle) -> Result<String, String> {
-    println!("I was invoked from JS!");
-    Ok("Hello world from Tauri!".into())
-}
+use tauri::AppHandle;
 
 #[tauri::command]
 pub async fn register_book_command(handle: tauri::AppHandle, new_book: NewBook) {
-    let app_datapath = app_local_data_dir(&handle.config()).unwrap();
-
-    let mut conn =
-        establish_connection(format!("{}/library.sqlite", app_datapath.to_str().unwrap()).as_str());
+    let conn = get_database_connection(&handle);
 
     register_book(&mut conn, &new_book);
+}
+
+#[tauri::command]
+pub async fn get_all_authors(handle: tauri::AppHandle) -> Vec<Author> {
+    let conn = get_database_connection(&handle);
+
+    get_authors(conn)
+}
+
+fn get_database_connection(handle: &AppHandle) -> SqliteConnection {
+    let app_datapath = app_local_data_dir(&handle.config()).unwrap();
+
+    establish_connection(format!("{}/library.sqlite", app_datapath.to_str().unwrap()).as_str())
 }
