6723bc0aa5708003d271002e6b238fa238875c30
Salvador Hernandez <salva.hg01@gmail.com>
2023-01-26 21:12:10 -0600

    Database: Use the database url from the environment in binaries

--- src-db/src/bin/create_book.rs
+++ src-db/src/bin/create_book.rs
@@ -1,3 +1,4 @@
+use std::env;
 use diesel::prelude::*;
 use src_db::models::books::{Book, NewBook};
 use src_db::*;
@@ -6,7 +7,11 @@ use src_db::models::authors::{NewAuthor};
 fn main() {
     use self::schema::books::dsl::*;
 
-    let connection = &mut establish_connection();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+
+    let connection = &mut establish_connection(&database_url);
 
     register_book(connection, &NewBook {
         condition: Some(2),
