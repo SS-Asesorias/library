3e81a8ac70a8a6b804fd0474bede8652547517c8
Salvador Hernandez <salva.hg01@gmail.com>
2023-02-07 20:38:48 -0600

    App: Little improvements for the side bar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { AddBookComponent } from './pages/add-book/add-book.component';
 import { HomeComponent } from './home.component';
@@ -10,7 +11,6 @@ import { InventoryComponent } from './pages/inventory/inventory.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatStepperModule } from '@angular/material/stepper';
@@ -23,6 +23,10 @@ import { MatListModule } from '@angular/material/list';
   declarations: [AddBookComponent, HomeComponent, InventoryComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     MatCardModule,
     MatButtonModule,
     MatInputModule,
@@ -34,8 +38,6 @@ import { MatListModule } from '@angular/material/list';
     MatCheckboxModule,
     MatSidenavModule,
     MatListModule,
-    RouterOutlet,
-    RouterLink,
   ],
 })
 export class HomeModule {}
