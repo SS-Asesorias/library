18fec897717d3ff45781a92646a3be738d65d553
Salvador Hernández <salva.hg01@gmail.com>
2023-02-12 01:45:45 -0600

    Database: Add bin for testing the new function to query all authors from a book

---
+++ src-db/src/bin/show_book_authors.rs
@@ -0,0 +1,20 @@
+use std::env;
+use dotenvy::dotenv;
+use src_db::{establish_connection, get_authors_by_book};
+
+fn main() {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
+    let connection = &mut establish_connection(&database_url);
+
+    let authors = get_authors_by_book(connection, 1);
+
+    for author in authors {
+        println!("{}", author.id);
+        println!("{}", author.name);
+        println!("{}", author.lname);
+    }
+}
