de61a7a74c589246a8225272c652df9cb14b8828
Salvador Hernández <salva.hg01@gmail.com>
2023-02-12 01:45:21 -0600

    Database: Code cleanup for some bins

--- src-db/src/bin/show_authors.rs
+++ src-db/src/bin/show_authors.rs
@@ -10,8 +10,6 @@ fn main() {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
 
     let connection = &mut establish_connection(&database_url);
     let results = authors
